c58d3cec95b1757744140fb4e55820562cbe2244
hsjeevan <hsjeevan07@gmail..com>
2018-07-16 11:21:55 +0530

    commit and push 2

--- app/src/main/java/com/hsjeevan/helloworldapp/MessageActivity.java
+++ app/src/main/java/com/hsjeevan/helloworldapp/MessageActivity.java
@@ -1,7 +1,10 @@
 package com.hsjeevan.helloworldapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MessageActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MessageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra("EXTRA_MESSAGE");
+        TextView textView = findViewById(R.id.display_message);
+        textView.setText(message);
     }
-}
+
+    public void openFinalActivity(View view) {
+
+        startActivity(new Intent(this,FinalActivity.class));
+    }
+}//testing
